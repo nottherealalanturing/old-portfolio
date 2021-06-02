fff858ff9ea6b96433c1a848fa65601df63997a1
Assad Isah <nottherealalanturing@gmail.com>
2021-06-02 06:26:40 +0100

    Updates

---
+++ component---src-pages-404-js-d31f9bda848a7fca4878.js
@@ -0,0 +1,2 @@
+(self.webpackChunknottherealalanturing=self.webpackChunknottherealalanturing||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var l=n(7294),u=n(4165),a=n(9470);t.default=function(){return l.createElement(u.Z,null,l.createElement(a.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d31f9bda848a7fca4878.js.map
